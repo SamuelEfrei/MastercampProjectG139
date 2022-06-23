069bdb160a55365481d0db490adc0eaa3c658ecd
adancingmilk <antoine.lachaud91620@gmail.com>
2022-06-23 15:06:44 +0200

    Nouvelle classe ajoutée

---
+++ MastercampProjectG139/PersonnelSante.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MastercampProjectG139
+{
+    internal class PersonnelSante
+    {
+        private String nom;
+        private String prenom;
+        private int numSecuSociale;
+
+        public PersonnelSante(String nom, String prenom, int numSecuSociale)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.numSecuSociale = numSecuSociale;
+        }
+    }
+}
